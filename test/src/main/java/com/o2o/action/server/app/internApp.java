7e70eeba30dfc2aa3ba969f7a7d41a57642911d4
OhWonjae <dhdnjswo5000@naver.com>
2020-07-15 14:55:01 +0900

    주석정리, 코드정리

--- test/src/main/java/com/o2o/action/server/app/internApp.java
+++ test/src/main/java/com/o2o/action/server/app/internApp.java
@@ -26,6 +26,7 @@ import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 public class internApp extends DialogflowApp {
+	//테스트 Main문
 	public static void main(String[] args) {
 		StringBuffer sb = new StringBuffer();
 		BufferedReader br = null;
@@ -72,32 +73,21 @@ public class internApp extends DialogflowApp {
 
 
 	}
-
+	//서류마음이 이미지 url
+	String MaumImageUrl = "https://mblogthumb-phinf.pstatic.net/MjAxOTExMThfMjAy/MDAxNTc0MDU0MDU3MzI3.pSSabICxa-7KDC8e0Kbe1zsPRGrT14qiuvjwKtBsAjkg.5xUHN3AwhVFYSUDY6SAq2gfVHCBPA1T7dGZAMFJBi7Yg.GIF.gbdc04/2.gif?type=w800";
 	@ForIntent("Default Welcome Intent")
 	public ActionResponse defaultWelcome(ActionRequest request) throws ExecutionException, InterruptedException {
 		ResponseBuilder rb = getResponseBuilder(request);
 		Map<String, Object> data = rb.getConversationData();
 
 		data.clear();
-
-		/*List<String> suggestions = new ArrayList<String>();
-		SimpleResponse simpleResponse = new SimpleResponse();
-		simpleResponse.setTextToSpeech("안녕하세요, 테스트앱 입니다.")
-				.setDisplayText("안녕하세요, 테스트앱 입니다.");
-			rb.add(simpleResponse);*/
 		return rb.build();
 	}
-	/*public ActionResponse RetError(ActionRequest request, String errorCode) throws ExecutionException, InterruptedException {
-	{
-		ResponseBuilder responseBuilder = getResponseBuilder(request);
 
-		SimpleResponse simpleResponse = new SimpleResponse();
-		simpleResponse.setTextToSpeech(errorCode)
-				.setDisplayText(errorCode);
-		responseBuilder.add(simpleResponse);
-		return responseBuilder.build();
-	}*/
+	//AdressConfirm 인텐트에서 받은 아이템리스트 담을 전역변수
 	ArrayList<ListSelectListItem> ItemList;
+
+	//입력된 주소와 매칭할 인텐트
 	@ForIntent("AdressConfirm")
 	public ActionResponse confirmAdress(ActionRequest request) throws ExecutionException, InterruptedException {
 				ResponseBuilder responseBuilder = getResponseBuilder(request);
@@ -110,13 +100,14 @@ public class internApp extends DialogflowApp {
 		// 사용자가 적은 주소
 		String adress = request.getRawText();
 
-		String currentPage = "1";
-		String countPerPage = "5";
+		String currentPage = "1"; //보여줄 페이지
+		String countPerPage = "100"; //페이지 당 보여줄 목록 갯수
 		String resultType = "json";
 		String confmKey = "devU01TX0FVVEgyMDIwMDcwOTE3MDUyMDEwOTk0MjQ=";
 		String keyword = adress;
 		String URLkeyword = "";
 		try {
+			//한글 인식 위해 UTF-8 추가
 			URLkeyword = URLEncoder.encode(keyword, "UTF-8");
 		} catch (UnsupportedEncodingException ec) {
 			ec.printStackTrace();
@@ -125,26 +116,38 @@ public class internApp extends DialogflowApp {
 		final String apiUrl = "http://www.juso.go.kr/addrlink/addrLinkApi.do?currentPage=" + currentPage
 				+ "&countPerPage=" + countPerPage + "&keyword=" + URLkeyword
 				+ "&confmKey=" + confmKey + "&resultType=" + resultType;
+		//Get 함수를 통한 RestApi 정보 받아오기
 		apiController api = new apiController();
 		String result = api.get(apiUrl);
-// 가장 큰 JSONObject를 가져옵니다.
+		//받아온 Json정보 파싱
 		JsonParser jsonParser = new JsonParser();
 		JsonObject jsonobj = (JsonObject) jsonParser.parse(result);
+		// 받아온 정보의 에러코드 확인
 		String errorCode = jsonobj.get("results").getAsJsonObject().get("common").getAsJsonObject().get("errorCode").getAsString();
 		String errorMessage = jsonobj.get("results").getAsJsonObject().get("common").getAsJsonObject().get("errorMessage").getAsString();
+		// 에러코드가 정상이 아니면 에러 메시지 출력 후 해당 인텐트 종료
 		if(!errorCode.equals("0"))
 		{
-			SimpleResponse simpleResponse = new SimpleResponse();
-			simpleResponse.setTextToSpeech(errorMessage + "다시 입력해 주세요.")
-					.setDisplayText(errorMessage + "다시 입력해 주세요.");
-			responseBuilder.add(simpleResponse);
+			responseBuilder
+					//에러 메시지 출력
+					.add(errorMessage)
+					.add(
+							new BasicCard()
+									//서류마음이 이미지 추가
+									.setImage(
+											new Image()
+													.setUrl(MaumImageUrl)
+													.setAccessibilityText("서류마음이")));
 			return responseBuilder.build();
 		}
+
 			JsonElement jsone = jsonobj.get("results").getAsJsonObject().get("juso");
 			String st = "";
 			List AdressList = new ArrayList();
+			//주소를 받아올때마다 ItemList 초기화
 			ItemList = new ArrayList<>();
 			String Spagenumber = jsonobj.get("results").getAsJsonObject().get("common").getAsJsonObject().get("totalCount").getAsString();
+			//받아온 결과주소가 1개면 베이직카드로 출력 후 해당 인텐트 종료
 			if(Integer.parseInt(Spagenumber)==1)
 			{
 				JsonObject obj = (JsonObject) jsone.getAsJsonArray().get(0);
@@ -152,6 +155,12 @@ public class internApp extends DialogflowApp {
 						.add("검색 결과입니다.")
 						.add(
 								new BasicCard()
+										//서류마음이 이미지 추가
+										.setImage(
+												new Image()
+														.setUrl(MaumImageUrl)
+														.setAccessibilityText("서류마음이"))
+										//내가 입력한 지번주와 대조 위해 완전한 지번주소를 타이틀로, 도로명주소를 내용으로 작성
 										.setTitle("지번주소 : "+obj.get("jibunAddr").toString()+"\n우편번호 : " + obj.get("zipNo").toString())
 										.setFormattedText("도로명 주소 : "+obj.get("roadAddr").toString())
 										.setButtons(
@@ -159,45 +168,52 @@ public class internApp extends DialogflowApp {
 														Arrays.asList(
 																new Button()
 																		.setTitle("지도로 이동")
-																		.setOpenUrlAction(
+																		.setOpenUrlAction( //베이직카드의 지도로 이동 버튼누르면 KakaoAPI의 지도 보기로 연결
 																				new OpenUrlAction().setUrl("https://map.kakao.com/link/search/"+obj.get("roadAddr")))))));
 				return responseBuilder.build();
 			}
+			// 리스트로 뿌릴 아이템 만든 후 AdressList와 ItemList에 추가
 			for (int i = 0; i <jsone.getAsJsonArray().size(); i++) {
 				JsonObject obj = (JsonObject) jsone.getAsJsonArray().get(i);
 				ListSelectListItem Item  = new ListSelectListItem();
 				Item.setDescription("지번주소 : "+obj.get("jibunAddr").toString()+"\n우편번호 : " + obj.get("zipNo").toString());
 				Item.setTitle("도로명 주소 : "+obj.get("roadAddr").toString());
 				OptionInfo optionInfo = new OptionInfo();
+				//옵션키는 받아온 리스트의 인덱스로 할당
 				optionInfo.setKey(Integer.toString(i));
 				Item.setOptionInfo(optionInfo);
 				AdressList.add(Item);
 				ItemList.add(Item);
 			}
+			//리스트 리턴
 			responseBuilder
-					.add("원하시는 도로명 주소를 선택해 주세요." + Spagenumber + " " +jsone.getAsJsonArray().size() )
+					.add("원하시는 도로명 주소를 선택해 주세요.")
 					.add(
 							new SelectionList()
 									.setTitle("도로명 주소 리스트")
 									.setItems(AdressList));
-/*		SimpleResponse simpleResponse = new SimpleResponse();
-		simpleResponse.setTextToSpeech(Integer.toString(jsone.getAsJsonArray().size()))
-				.setDisplayText(Integer.toString(jsone.getAsJsonArray().size()));
-		responseBuilder.add(simpleResponse);*/
 			return responseBuilder.build();
 		}
+
+		// AdressConfirm 인텐트에서 받아온 리스트를 선택했을때 매칭되는 인텐트
 		@ForIntent("ListSelection")
 		public ActionResponse listSelected (ActionRequest request){
 			ResponseBuilder responseBuilder = getResponseBuilder(request);
-
+			//리스트의 option key로 설정해둔 인덱스를 가져와서 ItemList에서 해당 인덱스를 이용하여 선택된 아이템 뽑아내기
 			String selectedItem = request.getSelectedOption();
 			int ItemIndex = Integer.parseInt(selectedItem);
 			ListSelectListItem item  = ItemList.get(ItemIndex);
-			// a line break to be rendered in the card.
+			//선택된 정보는 베이직카드로 리턴
 			responseBuilder
 					.add("검색 결과입니다.")
 					.add(
 							new BasicCard()
+									//서류마음이 이미지 추가
+									.setImage(
+											new Image()
+													.setUrl(MaumImageUrl)
+													.setAccessibilityText("서류마음이"))
+									//내가 입력한 지번주와 대조 위해 완전한 지번주소를 타이틀로, 도로명주소를 내용으로 작성
 									.setTitle(item.getDescription())
 									.setFormattedText(item.getTitle())
 									.setButtons(
@@ -205,9 +221,8 @@ public class internApp extends DialogflowApp {
 													Arrays.asList(
 															new Button()
 																	.setTitle("지도로 이동")
-																	.setOpenUrlAction(
+																	.setOpenUrlAction(//베이직카드의 지도로 이동 버튼누르면 KakaoAPI의 지도 보기로 연결
 																			new OpenUrlAction().setUrl("https://map.kakao.com/link/search/"+item.getTitle()))))));
-
 
 			return responseBuilder.build();
 		}
